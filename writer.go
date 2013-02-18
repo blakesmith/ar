2aae48b9646128c04ac899b12ffd1dc14578ca5b
Blake Smith <blakesmith0@gmail.com>
2013-02-17 22:57:08 -0600

    Basic writer

--- writer.go
+++ writer.go
@@ -3,7 +3,18 @@ package ar
 import (
 	"io"
 	"time"
+	"strconv"
 )
+
+const HEADER_BYTE_SIZE = 68 
+
+type slicer []byte
+
+func (sp *slicer) next(n int) (b []byte) {
+	s := *sp
+	b, *sp = s[0:n], s[n:]
+	return
+}
 
 type Writer struct {
 	w io.Writer
@@ -11,7 +22,7 @@ type Writer struct {
 }
 
 type Header struct {
-	FileName string
+	Name string
 	ModTime time.Time
 	Uid int
 	Gid int
@@ -21,18 +32,58 @@ type Header struct {
 
 func NewWriter(w io.Writer) *Writer { return &Writer{w: w} }
 
+func (aw *Writer) numeric(b []byte, x int64) {
+	s := strconv.FormatInt(x, 10)
+	for len(s) < len(b) {
+		s = s + " "
+	}
+	copy(b, []byte(s))
+}
+
+func (aw *Writer) octal(b []byte, x int64) {
+	s := "100" + strconv.FormatInt(x, 8)
+	for len(s) < len(b) {
+		s = s + " "
+	}
+	copy(b, []byte(s))
+}
+
+func (aw *Writer) string(b []byte, str string) {
+	s := str
+	for len(s) < len(b) {
+		s = s + " "
+	}
+	copy(b, []byte(s))
+}
+
 func (aw *Writer) Write(b []byte) (n int, err error) {
+	n, err = aw.w.Write(b)
+	if len(b)%2 == 1 {
+		n2, _ := aw.w.Write([]byte{'\n'})
+		return n+n2, err
+	}
+
 	return
 }
 
 func (aw *Writer) WriteHeader(hdr *Header) error {
+	header := make([]byte, HEADER_BYTE_SIZE)
+	s := slicer(header)
+
 	if !aw.globalHeader {
-		_, err := aw.w.Write([]byte("!<arch>\n"))
-		if err != nil {
-			return err
-		}
+		aw.string(s.next(8), "!<arch>\n")
 		aw.globalHeader = true
 	}
+
+	aw.string(s.next(16), hdr.Name)
+	aw.numeric(s.next(12), hdr.ModTime.Unix())
+	aw.numeric(s.next(6), int64(hdr.Uid))
+	aw.numeric(s.next(6), int64(hdr.Gid))
+	aw.octal(s.next(8), hdr.Mode)
+	aw.numeric(s.next(10), hdr.Size)
+	aw.string(s.next(2), "`\n")
+
+	aw.w.Write(header)
 
 	return nil
 }
--- writer_test.go
+++ writer_test.go
@@ -2,7 +2,10 @@ package ar
 
 import (
 	"bytes"
+	"io/ioutil"
+	"os"
 	"testing"
+	"time"
 )
 
 func TestGlobalHeaderWrite(t *testing.T) {
@@ -12,9 +15,38 @@ func TestGlobalHeaderWrite(t *testing.T) {
 		t.Errorf(err.Error())
 	}
 
-	globalHeader := buf.Bytes()
+	globalHeader := buf.Bytes()[0:8]
 	expectedHeader := []byte("!<arch>\n")
 	if !bytes.Equal(globalHeader, expectedHeader) {
 		t.Errorf("Global header should be %s but is %s", expectedHeader, globalHeader)
+	}
+}
+
+func TestSimpleFile(t *testing.T) {
+	hdr := new(Header)
+	body := "Hello world!\n"
+	hdr.ModTime = time.Unix(1361157466, 0)
+	hdr.Name = "hello.txt"
+	hdr.Size = int64(len(body))
+	hdr.Mode = 0644
+	hdr.Uid = 501
+	hdr.Gid = 20
+
+	var buf bytes.Buffer
+	writer := NewWriter(&buf)
+	writer.WriteHeader(hdr)
+	writer.Write([]byte(body))
+
+	f, _ := os.Open("./fixtures/hello.a")
+	defer f.Close()
+
+	b, err := ioutil.ReadAll(f)
+	if err != nil {
+		t.Errorf(err.Error())
+	}
+
+	actual := buf.Bytes()
+	if !bytes.Equal(b, actual) {
+		t.Errorf("Expected %s to equal %s", actual, b)
 	}
 }
